b93abc8d0a8733718cf1640ec99fe7e278d90052
Jens Buysse <jens.buysse@hogent.be>
2018-03-29 11:35:34 +0200

    Update even wat rare dingen toegevoegd

--- eerstetest.R
+++ eerstetest.R
@@ -1,3 +1,14 @@
 
 dataset <- read.csv("C:/Users/jensb/Documents/dataanalyse/dataanalyse/datasets/dataset-82167.csv")
 View(dataset.82167)
+
+
+# geef de namen van de variabelen weer
+names(dataset)
+
+
+
+# geef enkel de variabele hd weer
+dataset$hd
+
+;jejcejkfherkjfrkh
